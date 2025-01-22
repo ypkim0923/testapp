58838eaff87711b47da8da39bcf35eaeaf9ce989
ypkim <ypkim@autocrypt.io>
2025-01-22 15:48:22 +0900

    init commit

---
+++ src/main/java/com/ypkim/k8s/Sample/SampleApplication.java
@@ -0,0 +1,13 @@
+package com.ypkim.k8s.Sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+
+}
